a014e9d648315b6834cc619750e6e0a4560c8d1a
Gabriel Chagas <gabrielserq@outlook.com>
2022-07-13 11:23:11 -0300

    chore: retrieving the city and checking if the input is not empty.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
 
+    let cityValue = cityRef.value;
+    // If input field is empty
+    if(cityValue.length == 0) result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`
