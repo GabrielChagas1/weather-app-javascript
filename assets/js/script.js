82281d9d42be8a79ec672a3c42a33ebf4bb396aa
Gabriel Chagas <gabrielserq@outlook.com>
2022-07-13 11:25:45 -0300

    chore: finished script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
 
+// Function to fetch weather details from api and display them
+let getWeather = () => {
     let cityValue = cityRef.value;
     // If input field is empty
     if(cityValue.length == 0) result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`
+    else{
         // If input field is NOT empty
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`
         fetch(url).then((resp) => resp.json()).
