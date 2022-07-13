8ab42ad439c81bdb0df95344b63f240781e82871
Gabriel Chagas <gabrielserq@outlook.com>
2022-07-13 11:24:11 -0300

    chore: if the city is not empty and exists, the element is created.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,26 @@ let cityRef = document.getElementById("city");
     let cityValue = cityRef.value;
     // If input field is empty
     if(cityValue.length == 0) result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`
+        // If input field is NOT empty
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`
+        fetch(url).then((resp) => resp.json()).
+        then(data => {
+            // If city name is valid
+            console.log(data.weather[0].icon)
+            result.innerHTML = `
+            <h2>${data.name}</h2>
+            <h4 class="weather">${data.weather[0].main}</h4>
+            <h4 class="description">${data.weather[0].description}</h4>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            <h1>${data.main.temp} &#176</h1>
+            <div class="temp-container">
+                <div>
+                    <h4 class="title">min</h4>
+                    <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                </div>
+                <div>
+                    <h4 class="title">max</h4>
+                    <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                </div>
+            </div>
+            `
